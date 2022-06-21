9d33bb358f7dddf4d765db80c28fa485bf4c8818
Akemi Alice <86382666+akemialice@users.noreply.github.com>
2022-06-21 15:12:29 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,126 +1,79 @@
 using Microsoft.Data.Sqlite;
-
 using LabManager.Database;
 using LabManager.Repositories;
 using LabManager.Models;
 
 var databaseConfig = new DatabaseConfig();
+
 var databaseSetup = new DatabaseSetup(databaseConfig);
+
 var computerRepository = new ComputerRepository(databaseConfig);
 
-
-//Routing
+// Routing
 var modelName = args[0];
 var modelAction = args[1];
 
 if(modelName == "Computer")
 {
-
-    if(modelAction == "List")
-    {
+    var computerRepository = new ComputerRepository(databaseConfig);
+    if(modelAction == "List") {
         Console.WriteLine("Computer List");
-        foreach (var computer in computerRepository.GetAll())
-        {
+        foreach (var computer in computerRepository.GetAll()) {
             Console.WriteLine("{0}, {1}, {2}", computer.Id, computer.Ram, computer.Processor);
         }
     }
-
-    if(modelAction == "New")
-    {
-        Console.WriteLine("Computer New");
-        int id = Convert.ToInt32(args[2]);
-        var ram = args[3];
-        var processor = args[4];
-      
-        var computer = new Computer(id, ram, processor);
-        computerRepository.Save(computer);
-        Console.WriteLine("{0}, {1}, {2}", computer.Id, computer.Ram, computer.Processor);
-    }
-
-    if(modelAction == "Show")
-    {
-
-        int id = Convert.ToInt32(args[2]);
-    
-            if(computerRepository.existsById(id))
-        {
-            var computer = computerRepository.GetById(id);
-            Console.WriteLine("{0}, {1}, {2}", computer.Id, computer.Ram, computer.Processor);
-        } else {
-            Console.WriteLine($"O computador ${id} não existe");
-        }
-    }
-
-     if(modelAction == "Update")
-    {
-        int id = Convert.ToInt32(args[2]);
-      
-        if(computerRepository.ExistsById(id))
-        {
-            var ram = args[3];
-            var processor = args[4];
-            var computer = new Computer(id, ram, processor);
-            computerRepository.Update(computer); 
-        } else {
-            Console.WriteLine($"O computador {id} não existe!");
-        }   
-    }
-
-       if(modelAction == "Delete")
-    {
-
-        int id = Convert.ToInt32(args[2]);
-
-        if(computerRepository.ExistsById(id))
-        {
-            computerRepository.Delete(id);
-            Console.WriteLine($"Computador {id} deletado.");
-        } else {
-             Console.WriteLine($"O computador {id} não existe ou não foi encontrado.");
-        }  
-    }
-}
-
-else if(modelName == "Lab")
-{
-    if(modelAction == "List")
-    {
-        Console.WriteLine("Lab List");
-        var connection = new SqliteConnection("Data Source=database.db");
-        connection.Open();
-
-        var command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Labs;";
-
-        var reader = command.ExecuteReader();
-
-        while(reader.Read())
-        {
-            Console.WriteLine("{0}, {1}, {2}, {3}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
-        }
-
-        reader.Close();
-        connection.Close();
     }
 
     if(modelAction == "New")
     {
         var id = Convert.ToInt32(args[2]);
-        var number = args[3];
-        var name = args[4];
-        var block = args[5];
+        string ram = args[3];
+        string processor = args[4];
+        var computer = new Computer(id, ram, processor);
 
-        var connection = new SqliteConnection("Data Source=database.db");
-        connection.Open();
-
-        var command = connection.CreateCommand();
-        command.CommandText = "INSERT INTO Labs VALUES($id, $number, $name, $block);";
-        command.Parameters.AddWithValue("$id", id);
-        command.Parameters.AddWithValue("$number", number);
-        command.Parameters.AddWithValue("$name", name);
-        command.Parameters.AddWithValue("$block", block);
-
-        command.ExecuteNonQuery();
-        connection.Close();
+        computerRepository.Save(computer);
     }
-}
+
+    if(modelAction == "Show")
+    {
+        var id = Convert.ToInt32(args[2]);
+
+        if(computerRepository.ExitsById(id))
+        {
+            var computer = computerRepository.GetById(id);
+            Console.WriteLine($"{computer.Id}, {computer.Ram}, {computer.Processor}");
+        } 
+        else 
+        {
+            Console.WriteLine($"O computador com Id {id} não existe.");
+        }
+    }
+
+    if(modelAction == "Update")
+    {
+        var id = Convert.ToInt32(args[2]);
+        if(computerRepository.ExitsById(id))
+        {
+            string ram = args[3];
+            string processor = args[4];
+            var computer = new Computer(id, ram, processor);
+            computerRepository.Update(computer);
+        }
+        else
+        {
+            Console.WriteLine($"O computador com Id {id} não existe.");
+        }
+    }
+
+    if(modelAction == "Delete")
+    {
+        var id = Convert.ToInt32(args[2]);
+        if(computerRepository.ExitsById(id))
+        {
+            computerRepository.Delete(id);
+        }
+        else
+        {
+            Console.WriteLine($"O computador com Id {id} não existe.");
+        }        
+    }
