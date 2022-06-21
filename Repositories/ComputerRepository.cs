ba26464963bb776bfff44cbbfeed82a80b949aa0
Akemi Alice <86382666+akemialice@users.noreply.github.com>
2022-06-21 15:11:50 -0300

    Update ComputerRepository.cs

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -6,43 +6,45 @@ using Dapper;
 namespace LabManager.Repositories;
 
 class ComputerRepository
-{
+    {
     private DatabaseConfig databaseConfig;
-
     public ComputerRepository(DatabaseConfig databaseConfig) 
     {
         this.databaseConfig = databaseConfig;
     }
 
-    public IEnumerable<Computer> GetAll()
+//1
+public IEnumerable<Computer> GetAll()
     {
-        using var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        using var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
         var computers = connection.Query<Computer>("SELECT * FROM Computers");
         return computers;
     }
 
+
+    public Computer Save(Computer computer)
+    {
+        using var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+        connection.Execute("INSERT INTO Computers VALUES(@Id, @Ram, @Processor)",computer);
+        return computer;
+    }
+
+ //2   
     public Computer GetById(int id)
     {
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Open();
-        var computer = connection.QuerySingle<Computer>("SELECT * FROM Computers Where id = @Id ", new { Id = id });
+        var computer = connection.QuerySingle<Computer>("SELECT * FROM Computers WHERE id = @Id", new {Id = id});
         return computer;
     }
-
-    public Computer Save(Computer computer) 
-    {
-        using var connection = new SqliteConnection(databaseConfig.ConnectionString);
-        connection.Open();
-        connection.Execute("INSERT INTO Computers VALUES(@Id, @Ram, @Processor)", computer);
-        return computer;
-    }
-
+//3
     public Computer Update(Computer computer)
     {
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Open();
-        connection.Execute("UPDATE Computers SET id = @Id, ram= @Ram, processor = @Processor WHERE id = @Id ",computer);
+        connection.Execute("UPDATE Computers SET ram = @Ram, processor = @Processor WHERE id = @Id", computer);
         return computer;
     }
 
@@ -50,24 +52,16 @@ class ComputerRepository
     {
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Open();
-        connection.Execute("DELETE FROM Computers WHERE id = @Id", new { Id = id });
+        connection.Execute("DELETE FROM Computers WHERE id = @Id", new {Id = id});
     }
 
-    public bool ExistsById(int id)
+//4
+    public bool ExitsById(int id)
     {
         using var connection = new SqliteConnection(databaseConfig.ConnectionString);
         connection.Open();
-
         bool result = Convert.ToBoolean(connection.ExecuteScalar("SELECT count(id) FROM Computers WHERE id= @Id", new {Id = id}));
-        
         return result; 
-    }
-
-    private Computer readerToComputer(SqliteDataReader reader)
-    {
-        return new Computer
-        (reader.GetInt32(0), reader.GetString(1), reader.GetString(2)
-        );
     }
 
 }
