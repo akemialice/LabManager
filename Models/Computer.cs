4b1ded84ce6917fc4ffeccbcb55120bd4292b3a2
Akemi Alice <86382666+akemialice@users.noreply.github.com>
2022-06-21 10:57:07 -0300

    Update Computer.cs

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -5,8 +5,7 @@ class Computer
     public int Id { get; set; }
     public string Ram { get; set; }
     public string Processor { get; set; }
-
-    
+ public Computer() { }
     public Computer(int id, string ram, string processor)
     {
         Id = id;
